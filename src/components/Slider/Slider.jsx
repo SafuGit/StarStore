e890508861163f6b390aad5506f6a2f9429094f9
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-05-07 18:50:07 +0600

    updated carousel

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 const Slider = () => {
   return (
-    <div className='mb-50'>
+    <div className='mb-50 w-[90vw] mx-auto p-8 bg-[#1F2937] rounded-lg'>
       <Carousel autoPlay>
-        <img src="https://img.freepik.com/free-vector/banking-business-sale-banner-template_23-2150972725.jpg" alt="" />
+        <img src="https://cdn.dribbble.com/userupload/15932428/file/original-be0f189a148dc2f948fb4d79c05d8c8f.jpg?resize=1011x297&vertical=center" alt="" />
         <img src="https://cdn5.f-cdn.com/contestentries/2330502/20167322/654130ed4af30_thumb900.jpg" alt="" />
-        <img src="https://img.freepik.com/free-psd/food-online-concept-banner-mock-up_23-2148652328.jpg?semt=ais_hybrid&w=740" alt="" />
+        <img src="https://cdn.dribbble.com/userupload/24072560/file/original-a19159e3c64de75cdbd778d142a5abba.png?resize=1011x297&vertical=center" alt="" />
       </Carousel>
     </div>
   );
